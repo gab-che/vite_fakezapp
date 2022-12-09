6a2a6ba5c378a493389bf689f550e2db20231866
gab-che <gabriellachessa1@gmail.com>
2022-12-09 16:57:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+  @use './styles/general.scss';
+  @use './styles/partials/variables' as *;
+</style>
---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { reactive } from 'vue';
+import axios from 'axios';
+
+export const store = reactive({
+    userList: [],
+    messagesList: [],
+});
+
+export function fetchUsers() {
+
+};
+
+export function fetchMessages() {
+
+};
